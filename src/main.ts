6172db8ccd658b34279c7f7b143f532b6d27d0ba
Sarath S <sarathsuryasss48@gmail.com>
2024-12-02 00:48:39 +0530

    edited cors

--- src/main.ts
+++ src/main.ts
@@ -42,7 +42,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port');
   app.enableCors({
-    origin: 'https://6935-2401-4900-1cdc-69ff-a829-18d7-5e71-a3d1.ngrok-free.app/login',
+    origin: 'https://6935-2401-4900-1cdc-69ff-a829-18d7-5e71-a3d1.ngrok-free.app',
     credentials:true
   });
   app.use(cookieParser())

fca1c764d3907854ef6d6ace601127e088330a21
Sarath S <sarathsuryasss48@gmail.com>
2024-12-02 23:31:32 +0530

    for test

--- src/main.ts
+++ src/main.ts
@@ -45,7 +45,7 @@ async function bootstrap() {
     origin: 'https://perfect-pitch.site',
     credentials:true   
   });
-  // djjksfkjsfjsdfkjsdfhjksdfjkkkk
+  
   app.use(cookieParser())
   app.useGlobalFilters(new HttpExceptionFilter());
 
